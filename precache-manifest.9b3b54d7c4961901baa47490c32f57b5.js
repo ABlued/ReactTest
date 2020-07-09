8f5279aa7c5cba8e4bb5140913f44e59e537aeaa
ABlued <skwodhkswkd@naver.com>
2020-07-10 02:17:02 +0900

    Updates

---
+++ precache-manifest.9b3b54d7c4961901baa47490c32f57b5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "923455b46ee6c411ffa4c46a4f0f8a3d",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "dd021a573acc8d568d0b",
+    "url": "/movie_app_2020/static/css/main.35ad0c58.chunk.css"
+  },
+  {
+    "revision": "2e1c15df15ea19f85d25",
+    "url": "/movie_app_2020/static/js/2.38d42b34.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movie_app_2020/static/js/2.38d42b34.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd021a573acc8d568d0b",
+    "url": "/movie_app_2020/static/js/main.d874e749.chunk.js"
+  },
+  {
+    "revision": "1bb6a2b8272ceee248a1",
+    "url": "/movie_app_2020/static/js/runtime-main.064e30e1.js"
+  }
+]);
